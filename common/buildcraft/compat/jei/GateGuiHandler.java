9dc9247334d6c722316812c04012444b4acb2114
AlexIIL <alexj9@me.com>
2016-02-16 18:46:28 +0000

    Remove debug spam

--- common/buildcraft/compat/jei/GateGuiHandler.java
+++ common/buildcraft/compat/jei/GateGuiHandler.java
@@ -4,7 +4,6 @@ import java.awt.Rectangle;
 import java.util.ArrayList;
 import java.util.List;
 
-import buildcraft.api.core.BCLog;
 import buildcraft.transport.gui.GuiGateInterface;
 
 import mezz.jei.api.gui.IAdvancedGuiHandler;
@@ -18,7 +17,6 @@ public class GateGuiHandler implements IAdvancedGuiHandler<GuiGateInterface> {
     @Override
     public List<Rectangle> getGuiExtraAreas(GuiGateInterface gate) {
         List<Rectangle> rectangles = new ArrayList<>();
-        BCLog.logger.info("GATE[" + gate.actionRows + "," + gate.lastActionRowSize + "]");
         int guiLeft = (gate.width - gate.getXSize()) / 2;
         int guiTop = (gate.height - gate.getYSize()) / 2;
 
@@ -40,10 +38,8 @@ public class GateGuiHandler implements IAdvancedGuiHandler<GuiGateInterface> {
             int endRow = gate.actionRows * 18;
             rectangles.add(new Rectangle(actionStartX, actionStartY, 6 * 18, endRow));
             rectangles.add(new Rectangle(actionStartX, actionStartY + endRow, gate.lastActionRowSize, 18));
-            BCLog.logger.info("Added 2 actionRow rectanges! " + rectangles.get(0) + ", " + rectangles.get(1));
         } else if (gate.actionRows == 1) {
             rectangles.add(new Rectangle(actionStartX, actionStartY, gate.lastActionRowSize, 18));
-            BCLog.logger.info("Added 1 actionRow rectange! " + rectangles.get(0));
         }
         return rectangles;
     }
