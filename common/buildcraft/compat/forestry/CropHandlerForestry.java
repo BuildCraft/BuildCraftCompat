0e78cce8fd52b2a956dcf1361277cb737e597c75
asiekierka <asiekierka@gmail.com>
2016-03-18 14:11:04 +0100

    BCCompat 7.1.4, fix Forestry saplings

--- common/buildcraft/compat/forestry/CropHandlerForestry.java
+++ common/buildcraft/compat/forestry/CropHandlerForestry.java
@@ -1,18 +1,19 @@
 package buildcraft.compat.forestry;
 
-import buildcraft.api.core.BuildCraftAPI;
-import buildcraft.api.crops.ICropHandler;
-import forestry.api.arboriculture.EnumGermlingType;
-import forestry.api.arboriculture.ITree;
-import forestry.api.arboriculture.ITreeRoot;
-import forestry.api.genetics.AlleleManager;
+import java.util.List;
+
 import net.minecraft.block.Block;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
-import java.util.List;
+import buildcraft.api.crops.ICropHandler;
+
+import forestry.api.arboriculture.EnumGermlingType;
+import forestry.api.arboriculture.ITree;
+import forestry.api.arboriculture.ITreeRoot;
+import forestry.api.genetics.AlleleManager;
 
 public class CropHandlerForestry implements ICropHandler {
 	private static ITreeRoot treeRoot;
@@ -32,9 +33,9 @@ public class CropHandlerForestry implements ICropHandler {
 
 	@Override
 	public boolean canSustainPlant(World world, ItemStack itemStack, int x, int y, int z) {
-		if (world.isAirBlock(x, y, z) && isSeed(itemStack)) {
+		if (world.isAirBlock(x, y + 1, z) && isSeed(itemStack)) {
 			ITree tree = treeRoot.getMember(itemStack);
-			return tree != null && tree.canStay(world, x, y, z);
+			return tree != null && tree.canStay(world, x, y + 1, z);
 		} else {
 			return false;
 		}
@@ -42,8 +43,8 @@ public class CropHandlerForestry implements ICropHandler {
 
 	@Override
 	public boolean plantCrop(World world, EntityPlayer entityPlayer, ItemStack itemStack, int x, int y, int z) {
-		if (world.isAirBlock(x, y, z) && isSeed(itemStack)) {
-			if (treeRoot.plantSapling(world, treeRoot.getMember(itemStack), null, x, y, z)) {
+		if (world.isAirBlock(x, y + 1, z) && isSeed(itemStack)) {
+			if (treeRoot.plantSapling(world, treeRoot.getMember(itemStack), null, x, y + 1, z)) {
 				itemStack.stackSize--;
 				return true;
 			}
