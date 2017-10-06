b6970e4af343dc5f94b2af1708c43d0064629eda
faceofcat <candreix@gmail.com>
2017-10-06 09:05:11 +0300

    IAutoCraft and ILaserTarget integrations for HWYLA

---
+++ common/buildcraft/compat/waila/BaseWailaDataProvider.java
@@ -0,0 +1,45 @@
+package buildcraft.compat.waila;
+
+import java.util.List;
+import javax.annotation.Nonnull;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import mcp.mobius.waila.api.IWailaConfigHandler;
+import mcp.mobius.waila.api.IWailaDataAccessor;
+import mcp.mobius.waila.api.IWailaDataProvider;
+
+class BaseWailaDataProvider implements IWailaDataProvider {
+    @Nonnull
+    @Override
+    public ItemStack getWailaStack(IWailaDataAccessor accessor, IWailaConfigHandler config) {
+        return ItemStack.EMPTY;
+    }
+
+    @Nonnull
+    @Override
+    public List<String> getWailaHead(ItemStack itemStack, List<String> currentTip, IWailaDataAccessor accessor, IWailaConfigHandler config) {
+        return currentTip;
+    }
+
+    @Nonnull
+    @Override
+    public List<String> getWailaBody(ItemStack itemStack, List<String> currentTip, IWailaDataAccessor accessor, IWailaConfigHandler config) {
+        return currentTip;
+    }
+
+    @Nonnull
+    @Override
+    public List<String> getWailaTail(ItemStack itemStack, List<String> currentTip, IWailaDataAccessor accessor, IWailaConfigHandler config) {
+        return currentTip;
+    }
+
+    @Nonnull
+    @Override
+    public NBTTagCompound getNBTData(EntityPlayerMP player, TileEntity te, NBTTagCompound tag, World world, BlockPos pos) {
+        return tag;
+    }
+}
